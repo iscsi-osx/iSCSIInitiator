d1104dade8e2692d31790398f84f15f8efa1362e
Nareg Sinenian <nareg@alum.mit.edu>
2015-09-08 20:43:48 +0900

    Addition of target management using SendTargets discovery.

---
+++ User Tools/iSCSISendTargetsDiscovery.h
@@ -0,0 +1,20 @@
+/*!
+ * @author		Nareg Sinenian
+ * @file		iSCSISendTargetsDiscovery.h
+ * @version		1.0
+ * @copyright	(c) 2014-2015 Nareg Sinenian. All rights reserved.
+ * @brief		Discovery functions for use by iscsid.
+ */
+
+
+#ifndef __ISCSI_SENDTARGETS_DISCOVERY_H__
+#define __ISCSI_SENDTARGETS_DISCOVERY_H__
+
+#include <CoreFoundation/CoreFoundation.h>
+
+#include "iSCSISession.h"
+#include "iSCSITypes.h"
+#include "iSCSIPropertyList.h"
+
+
+#endif /* defined(__ISCSI_SENDTARGETS_DISCOVERY_H__) */
