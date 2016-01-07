4b1014e2b6e8e505ed602a735c19dee9818ccfb2
jonathan lung <auto+gpg@heresjono.com>
2016-01-06 22:57:27 -0500

    Install Xcode command-line tools if not already installed.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+if [ ! -f /usr/bin/xcrun ]; then
+    echo "Xcode command-line tools not installed. Starting the installer."
+    sudo xcode-select --install
+    echo "Please rerun the build command."
+    exit 1
+fi
 xcodebuild -scheme iSCSIDaemon build
 xcodebuild -scheme iscsictl build
 xcodebuild -scheme iSCSIInitiator build
