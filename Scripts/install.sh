3bbb9cec4e2972a947ddc7519bfa741ceede7860
Nareg Sinenian <nareg@alum.mit.edu>
2016-02-11 21:26:19 +0800

    Modified installation path for OS X Mavericks and earlier.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,6 +5,18 @@ DAEMON=iscsid
 TOOL=iscsictl
 KEXT=iSCSIInitiator.kext
 
+# Get minor version of the OS
+OSX_MINOR_VER=$(sw_vers -productVersion | awk -F '.' '{print $2}')
+
+# Minor version of OS X Mavericks
+OSX_MAVERICKS_MINOR_VER="9"
+
+if [ "$OSX_MINOR_VER" -ge "$OSX_MAVERICKS_MINOR_VER" ]; then
+    KEXT_DST=/Library/Extensions
+else
+    KEXT_DST=/System/Library/Extensions
+fi
+
 # Look for build products in places Xcode might place them.
 for BUILD_PATH in \
             ../DerivedData/Build/Products/Debug \
@@ -23,18 +35,18 @@ if [ X"" == X"${SOURCE_PATH}" ]; then
 fi
 
 # Copy kernel extension & load it
-sudo cp -R $SOURCE_PATH/$KEXT /Library/Extensions/$KEXT
-sudo chmod -R 755 /Library/Extensions/$KEXT
-sudo chown -R root:wheel /Library/Extensions/$KEXT
+sudo cp -R $SOURCE_PATH/$KEXT $KEXT_DST/$KEXT
+sudo chmod -R 755 $KEXT_DST/$KEXT
+sudo chown -R root:wheel $KEXT_DST/$KEXT
 
 # Copy daemon & set permissions
 sudo rm -f /var/logs/iscsid.log
 sudo cp $SOURCE_PATH/$DAEMON /Library/PrivilegedHelperTools/$DAEMON
-sudo cp $SOURCE_PATH/com.github.iscsi-osx.iscsid.plist /System/Library/LaunchDaemons
+sudo cp $SOURCE_PATH/com.github.iscsi-osx.iscsid.plist /Library/LaunchDaemons
 sudo chmod -R 744 /Library/PrivilegedHelperTools/$DAEMON
 sudo chown -R root:wheel /Library/PrivilegedHelperTools/$DAEMON
-sudo chmod 644 /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
-sudo chown root:wheel /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
+sudo chmod 644 /Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
+sudo chown root:wheel /Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
 
 # Copy user tool
 sudo cp $SOURCE_PATH/$TOOL /usr/local/bin/$TOOL
@@ -48,5 +60,5 @@ sudo cp $SOURCE_PATH/iscsid.8 /usr/share/man/man8
 sudo kextload /Library/Extensions/$KEXT
 
 # Start daemon
-sudo launchctl load /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
+sudo launchctl load /Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
 sudo launchctl start com.github.iscsi-osx.iscsid
--- Scripts/uninstall.sh
+++ Scripts/uninstall.sh
@@ -5,16 +5,28 @@ DAEMON=iscsid
 TOOL=iscsictl
 KEXT=iSCSIInitiator.kext
 
+# Get minor version of the OS
+OSX_MINOR_VER=$(sw_vers -productVersion | awk -F '.' '{print $2}')
+
+# Minor version of OS X Mavericks
+OSX_MAVERICKS_MINOR_VER="9"
+
+if [ "$OSX_MINOR_VER" -ge "$OSX_MAVERICKS_MINOR_VER" ]; then
+    KEXT_DST=/Library/Extensions
+else
+    KEXT_DST=/System/Library/Extensions
+fi
+
 # Stop, unload and remove launch daemon
-sudo launchctl stop /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid
-sudo launchctl unload /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
-sudo rm -f /System/Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
+sudo launchctl stop /Library/LaunchDaemons/com.github.iscsi-osx.iscsid
+sudo launchctl unload /Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
+sudo rm -f /Library/LaunchDaemons/com.github.iscsi-osx.iscsid.plist
 sudo rm -f /usr/sbin/$DAEMON # Old location
 sudo rm -f /Library/PrivilegedHelperTools/$DAEMON
 
 # Unload & remove kernel extension
-sudo kextunload /Library/Extensions/$KEXT
-sudo rm -f -R /Library/Extensions/$KEXT
+sudo kextunload $KEXT_DST/$KEXT
+sudo rm -f -R $KEXT_DST/$KEXT
 
 # Remove user tools
 sudo rm -f /usr/bin/$TOOL # Old location
